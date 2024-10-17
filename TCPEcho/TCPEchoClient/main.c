570cde2e914b4e308c11448197698d71f663ef4e
AengChoon <kevin1131@naver.com>
2024-10-17 17:54:54 +0900

    TCPEchoClient v2

--- TCPEcho/TCPEchoClient/main.c
+++ TCPEcho/TCPEchoClient/main.c
@@ -25,26 +25,6 @@ int main(int Argc, char* Argv[])
         DieWithUserMessage("SetupTCPClientSocket() failed", "unable to connect");
     }
 
-    struct sockaddr_in ServerAddress; // information container for address and port to connect to
-    memset(&ServerAddress, 0, sizeof(ServerAddress));
-    ServerAddress.sin_family = AF_INET; // address family
-
-    int ReturnValue = inet_pton(AF_INET, Server, &ServerAddress.sin_addr.s_addr); // convert IP representation from string to 32-bit binary
-    if (ReturnValue == 0)
-    {
-        DieWithUserMessage("inet_pton() failed", "invalid address string");
-    }
-    else if (ReturnValue < 0)
-    {
-        DieWithSystemMessage("inet_pton() failed");
-    }
-    ServerAddress.sin_port = htons(ServerPort); // convert IP port number in host byte order to network byte order
-
-    if (connect(ClientSocket, (struct sockaddr*)&ServerAddress, sizeof(ServerAddress)) < 0)
-    {
-        DieWithSystemMessage("connect() failed");
-    }
-
     size_t EchoStringLength = strlen(EchoString);
 
     ssize_t NumBytes = send(ClientSocket, EchoString, EchoStringLength, 0);
